61a89432c609c39719501df76d040ddd7f7509aa
Martin Junghanns <martin.junghanns@neo4j.com>
2020-12-25 11:00:21 +0100

    Organize project as workspace instead of modules

---
+++ smart_pointers/src/lib.rs
@@ -0,0 +1,15 @@
+//! Chapter 15: Smart pointers
+
+pub use self::cell::Cell;
+pub use self::rc::Rc;
+pub use self::refcell::RefCell;
+
+mod box_example;
+mod cell;
+mod rc;
+mod rc_example;
+mod refcell;
+mod refcell_example;
+mod reference_cycle;
+mod weak_references;
+mod weak;
