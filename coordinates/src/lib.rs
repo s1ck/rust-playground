9fae27a51412de9b28fd66bf725f7a6d9ab97a27
Martin Junghanns <martin.junghanns@neo4j.com>
2021-08-17 12:19:49 +0200

    Support f32 and f64 in coordinates

--- coordinates/src/lib.rs
+++ coordinates/src/lib.rs
@@ -1,50 +1,103 @@
-#[derive(Debug, PartialEq)]
-struct Point {
-    x: f64,
-    y: f64,
+#![allow(mixed_script_confusables)]
+
+trait Float: Copy + std::ops::Add<Output = Self> + std::ops::Mul<Output = Self> {
+    fn square(&self) -> Self;
+    fn atan(&self) -> Self;
+    fn atan2(&self, other: Self) -> Self;
+    fn cos(&self) -> Self;
+    fn sin(&self) -> Self;
+    fn sqrt(&self) -> Self;
 }
 
-impl Point {
-    fn new(x: f64, y: f64) -> Self {
+impl Float for f32 {
+    fn square(&self) -> Self {
+        f32::powf(*self, 2.0)
+    }
+
+    fn atan(&self) -> Self {
+        f32::atan(*self)
+    }
+
+    fn atan2(&self, other: Self) -> Self {
+        f32::atan2(*self, other)
+    }
+
+    fn cos(&self) -> Self {
+        f32::cos(*self)
+    }
+
+    fn sin(&self) -> Self {
+        f32::sin(*self)
+    }
+
+    fn sqrt(&self) -> Self {
+        f32::sqrt(*self)
+    }
+}
+
+impl Float for f64 {
+    fn square(&self) -> Self {
+        f64::powf(*self, 2.0)
+    }
+
+    fn atan(&self) -> Self {
+        f64::atan(*self)
+    }
+
+    fn atan2(&self, other: Self) -> Self {
+        f64::atan2(*self, other)
+    }
+
+    fn cos(&self) -> Self {
+        f64::cos(*self)
+    }
+
+    fn sin(&self) -> Self {
+        f64::sin(*self)
+    }
+
+    fn sqrt(&self) -> Self {
+        f64::sqrt(*self)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T: Float> Point<T> {
+    fn new(x: T, y: T) -> Self {
         Self { x, y }
     }
 }
 
 #[derive(Debug, PartialEq)]
-struct Polar {
-    r: f64,
-    phi: f64,
+struct Polar<T> {
+    r: T,
+    φ: T,
 }
 
-impl Polar {
-    fn new(r: f64, phi: f64) -> Self {
-        Self { r, phi }
+impl<T: Float> Polar<T> {
+    fn new(r: T, φ: T) -> Self {
+        Self { r, φ }
     }
 }
 
-impl From<Polar> for Point {
-    fn from(p: Polar) -> Self {
-        let x = p.r * p.phi.cos();
-        let y = p.r * p.phi.sin();
+impl<T: Float> From<Polar<T>> for Point<T> {
+    fn from(Polar { r, φ }: Polar<T>) -> Self {
+        let x = r * φ.cos();
+        let y = r * φ.sin();
         Self::new(x, y)
     }
 }
 
-impl From<Point> for Polar {
-    fn from(p: Point) -> Self {
-        let r = (p.x.square() + p.y.square()).sqrt();
-        let phi = p.y.atan2(p.x);
-        Self::new(r, phi)
-    }
-}
-
-trait Square {
-    fn square(&self) -> Self;
-}
-
-impl Square for f64 {
-    fn square(&self) -> Self {
-        self.powf(2.0)
+impl<T: Float> From<Point<T>> for Polar<T> {
+    fn from(Point { x, y }: Point<T>) -> Self {
+        let r = (x.square() + y.square()).sqrt();
+        let φ = y.atan2(x);
+        Self::new(r, φ)
     }
 }
 
@@ -53,16 +106,30 @@ mod tests {
     use super::*;
 
     #[test]
-    fn from_point() {
+    fn from_point_f64() {
         let point = Point::new(3.0, 4.0);
-        let polar: Polar = point.into();
+        let polar: Polar<_> = point.into();
         assert_eq!(polar, Polar::new(5.0, 0.9272952180016122));
     }
 
     #[test]
-    fn from_polar() {
+    fn from_polar_f64() {
         let polar = Polar::new(5.0, 0.9272952180016122);
-        let point: Point = polar.into();
+        let point: Point<_> = polar.into();
+        assert_eq!(point, Point::new(3.0000000000000004, 3.9999999999999996));
+    }
+
+    #[test]
+    fn from_point_f32() {
+        let point = Point::new(3.0_f32, 4.0_f32);
+        let polar: Polar<_> = point.into();
+        assert_eq!(polar, Polar::new(5.0, 0.9272952180016122));
+    }
+
+    #[test]
+    fn from_polar_f32() {
+        let polar = Polar::new(5.0_f32, 0.9272952180016122_f32);
+        let point: Point<_> = polar.into();
         assert_eq!(point, Point::new(3.0000000000000004, 3.9999999999999996));
     }
 }
