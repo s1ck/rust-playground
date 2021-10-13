ecd8baae417503d86a1f107d16c2bf736d0eb03f
Martin Junghanns <martin.junghanns@neo4j.com>
2021-10-13 08:12:12 +0200

    scan memory example

--- data_in_depth/src/memory.rs
+++ data_in_depth/src/memory.rs
@@ -87,6 +87,54 @@ fn ffi() {
     println!("a = {}, b = {}, c = {}", a, b, c);
 }
 
+fn scan_memory() {
+    let mut n_nonzero = 0;
+
+    for i in 1..10_000 {
+        let ptr = i as *const u8;
+
+        let byte_at_addr = unsafe { *ptr };
+
+        if byte_at_addr != 0 {
+            n_nonzero += 1;
+        }
+    }
+
+    println!("non-zero bytes in memory: {}", n_nonzero);
+}
+
+#[test]
+fn test_scan_memory() {
+    scan_memory();
+}
+
+static GLOBAL: i64 = 1000;
+
+fn noop() -> *const i32 {
+    let noop_local = 12345;
+    &noop_local as *const _
+}
+
+fn addresses() {
+    let local_str = "a";
+    let local_int = 123;
+    let boxed_str = Box::new("b");
+    let boxed_int = Box::new(789);
+    let fn_int = noop();
+
+    println!("GLOBAL    {:p}", &GLOBAL as *const i64);
+    println!("local_str {:p}", local_str as *const str);
+    println!("local_int {:p}", &local_int as *const i32);
+    println!("boxed_str {:p}", Box::into_raw(boxed_str));
+    println!("boxed_int {:p}", Box::into_raw(boxed_int));
+    println!("fn_int    {:p}", fn_int);
+}
+
+#[test]
+fn test_addresses() {
+    addresses();
+}
+
 #[test]
 fn run() {
     print_layout();
@@ -96,13 +144,11 @@ fn run() {
     unsafe_wtf();
 }
 
-struct Foo {
-
-}
+struct Foo {}
 
 #[test]
 fn feature() {
-    let x= std::mem::size_of::<Foo>();
+    let x = std::mem::size_of::<Foo>();
 
     println!("{}", x);
 }
