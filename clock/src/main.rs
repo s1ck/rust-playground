623b5697ceb42a8a2e6117e8712448b084ed74c1
Martin Junghanns <martin.junghanns@neo4j.com>
2021-10-27 21:04:21 +0200

    Clock example

---
+++ clock/src/main.rs
@@ -0,0 +1,92 @@
+use std::mem::zeroed;
+
+use chrono::{DateTime, Local, TimeZone};
+use clap::{App, Arg};
+
+use libc::{settimeofday, timezone};
+use libc::{suseconds_t, time_t, timeval};
+
+struct Clock;
+
+impl Clock {
+    fn get() -> DateTime<Local> {
+        Local::now()
+    }
+
+    fn set<Tz: TimeZone>(t: DateTime<Tz>) {
+        let t = t.with_timezone(&Local);
+        let mut u: timeval = unsafe { zeroed() };
+
+        u.tv_sec = t.timestamp() as time_t;
+        u.tv_usec = t.timestamp_subsec_micros() as suseconds_t;
+
+        unsafe {
+            let mock_tz: *const timezone = std::ptr::null();
+            settimeofday(&u as *const timeval, mock_tz);
+        }
+    }
+}
+
+fn main() {
+    let app = App::new("clock")
+        .version("0.1")
+        .about("Gets and sets the time")
+        .arg(
+            Arg::with_name("action")
+                .takes_value(true)
+                .possible_values(&["get", "set"])
+                .default_value("get"),
+        )
+        .arg(
+            Arg::with_name("std")
+                .short("s")
+                .long("standard")
+                .takes_value(true)
+                .possible_values(&["rfc2822", "rfc3339", "timestamp"])
+                .default_value("rfc3339"),
+        )
+        .arg(Arg::with_name("datetime").help(
+            "When <action> is 'set', apply <datetime>. \
+            Otherwise, ignore.",
+        ));
+
+    let args = app.get_matches();
+
+    let action = args.value_of("action").unwrap();
+    let std = args.value_of("std").unwrap();
+
+    if action == "set" {
+        let t_ = args.value_of("datetime").unwrap();
+
+        let parser = match std {
+            "rfc2822" => DateTime::parse_from_rfc2822,
+            "rfc3339" => DateTime::parse_from_rfc3339,
+            _ => unreachable!(),
+        };
+
+        let msg = format!("Unable to parse {} according to {}", t_, std);
+        let t = parser(t_).expect(&msg);
+
+        println!("attempting to set clock to {}", t);
+
+        Clock::set(t);
+
+        let maybe_error = std::io::Error::last_os_error();
+        let os_error_code = &maybe_error.raw_os_error();
+
+        match os_error_code {
+            Some(0) => (),
+            Some(_) => eprintln!("Unable to set the time: {:?}", maybe_error),
+            None => (),
+        }
+    }
+
+    let now = Clock::get();
+
+    match std {
+        "timestamp" => println!("{}", now.timestamp()),
+        "rfc2822" => println!("{}", now.to_rfc2822()),
+        "rfc3339" => println!("{}", now.to_rfc3339()),
+        _ => unreachable!(),
+    }
+}
--- codewars/src/lib.rs
+++ codewars/src/lib.rs
@@ -1,3 +1,8 @@
+#![allow(dead_code)]
+#![allow(unused)]
+
+use std::cmp::Ordering;
+
 fn dna_strand(dna: &str) -> String {
     dna.chars()
         .map(|c| match c {
@@ -86,4 +91,116 @@ fn test_persistence() {
     assert_eq!(persistence(4), 0);
     assert_eq!(persistence(25), 2);
     assert_eq!(persistence(999), 4);
+}
+
+fn longest(a1: &str, a2: &str) -> String {
+    let mut concat = format!("{}{}", a1, a2).chars().collect::<Vec<_>>();
+    concat.sort();
+    concat.dedup();
+    concat.iter().collect()
+}
+
+fn test_longest(s1: &str, s2: &str, exp: &str) -> () {
+    println!("s1:{:?} s2:{:?}", s1, s2);
+    println!("{:?} {:?}", longest(s1, s2), exp);
+    println!("{}", longest(s1, s2) == exp);
+    assert_eq!(&longest(s1, s2), exp)
+}
+
+#[test]
+fn test_longest_2() {
+    test_longest("aretheyhere", "yestheyarehere", "aehrsty");
+    test_longest(
+        "loopingisfunbutdangerous",
+        "lessdangerousthancoding",
+        "abcdefghilnoprstu",
+    );
+}
+
+fn sort_array(arr: &[i32]) -> Vec<i32> {
+    let even = arr
+        .into_iter()
+        .cloned()
+        .enumerate()
+        .filter(|(pos, n)| n % 2 == 0)
+        .collect::<Vec<_>>();
+
+    let mut odds = arr
+        .into_iter()
+        .cloned()
+        .filter(|&n| n % 2 != 0)
+        .collect::<Vec<_>>();
+
+    odds.sort_unstable();
+
+    for (i, n) in even {
+        odds.insert(i, n);
+    }
+
+    odds
+}
+
+#[test]
+fn tset_sort_array() {
+    assert_eq!(sort_array(&[5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
+    assert_eq!(sort_array(&[5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
+    assert_eq!(sort_array(&[]), []);
+}
+
+fn is_prime(x: i64) -> bool {
+    if x <= 1 {
+        return false;
+    }
+    if x == 2 {
+        return true;
+    }
+    if x % 2 == 0 {
+        return false;
+    }
+
+    let b = (x as f64).sqrt().floor() as i64;
+
+    for i in (3..=b).step_by(2) {
+        if x % i == 0 {
+            return false;
+        }
+    }
+
+    true
+}
+
+#[cfg(test)]
+mod is_prime_tests {
+    use super::*;
+
+    #[test]
+    fn basic_tests() {
+        assert!(!is_prime(0), "0 is not prime");
+        assert!(!is_prime(1), "1 is not prime");
+        assert!(is_prime(2), "2 is prime");
+        assert!(is_prime(73), "73 is prime");
+        assert!(!is_prime(75), "75 is not prime");
+        assert!(!is_prime(-1), "-1 is not prime");
+    }
+
+    #[test]
+    fn prime_tests() {
+        assert!(is_prime(3), "3 is prime");
+        assert!(is_prime(5), "5 is prime");
+        assert!(is_prime(7), "7 is prime");
+        assert!(is_prime(41), "41 is prime");
+        assert!(is_prime(5099), "5099 is prime");
+    }
+
+    #[test]
+    fn not_prime_tests() {
+        assert!(!is_prime(4), "4 is not prime");
+        assert!(!is_prime(6), "6 is not prime");
+        assert!(!is_prime(8), "8 is not prime");
+        assert!(!is_prime(9), "9 is not prime");
+        assert!(!is_prime(45), "45 is not prime");
+        assert!(!is_prime(-5), "-5 is not prime");
+        assert!(!is_prime(-8), "-8 is not prime");
+        assert!(!is_prime(-41), "-41 is not prime");
+    }
 }
