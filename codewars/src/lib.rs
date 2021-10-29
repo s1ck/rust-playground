164b216efc4d9ff0cb3c34821e5f4265b4760c85
Martin Junghanns <martin.junghanns@neo4j.com>
2021-10-29 16:31:35 +0200

    More katas

--- codewars/src/lib.rs
+++ codewars/src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 #![allow(unused)]
 
-use std::cmp::Ordering;
+use std::{cmp::Ordering, collections::HashMap};
 
 fn dna_strand(dna: &str) -> String {
     dna.chars()
@@ -203,4 +203,214 @@ mod is_prime_tests {
         assert!(!is_prime(-8), "-8 is not prime");
         assert!(!is_prime(-41), "-41 is not prime");
     }
+}
+
+fn alphabet_position(text: &str) -> String {
+    text.to_ascii_uppercase()
+        .chars()
+        .filter_map(|c| {
+            if c >= 'A' && c <= 'Z' {
+                Some((c as u32 - 64).to_string())
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
+#[cfg(test)]
+mod test_alphabet_position {
+    use super::*;
+
+    #[test]
+    fn returns_expected() {
+        assert_eq!(
+            alphabet_position("The sunset sets at twelve o' clock."),
+            "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11".to_string()
+        );
+        assert_eq!(
+            alphabet_position("The narwhal bacons at midnight."),
+            "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20".to_string()
+        );
+    }
+}
+
+fn sum_pairs(ints: &[i8], s: i8) -> Option<(i8, i8)> {
+    let mut seen = [false; 256];
+
+    for x in ints {
+        let y = s - x;
+        if seen[y as u8 as usize] {
+            return Some((y, *x));
+        }
+        seen[*x as u8 as usize] = true;
+    }
+
+    None
+}
+
+#[test]
+fn test_sum_pairs() {
+    let l1 = [1, 4, 8, 7, 3, 15];
+    let l2 = [1, -2, 3, 0, -6, 1];
+    let l3 = [20, -13, 40];
+    let l4 = [1, 2, 3, 4, 1, 0];
+    let l5 = [10, 5, 2, 3, 7, 5];
+    let l6 = [4, -2, 3, 3, 4];
+    let l7 = [0, 2, 0];
+    let l8 = [5, 9, 13, -3];
+    assert_eq!(sum_pairs(&l1, 8), Some((1, 7)));
+    assert_eq!(sum_pairs(&l2, -6), Some((0, -6)));
+    assert_eq!(sum_pairs(&l3, -7), None);
+    assert_eq!(sum_pairs(&l4, 2), Some((1, 1)));
+    assert_eq!(sum_pairs(&l5, 10), Some((3, 7)));
+    assert_eq!(sum_pairs(&l6, 8), Some((4, 4)));
+    assert_eq!(sum_pairs(&l7, 0), Some((0, 0)));
+    assert_eq!(sum_pairs(&l8, 10), Some((13, -3)));
+}
+
+fn spin_words(words: &str) -> String {
+    words
+        .split_whitespace()
+        .map(|w| match w.len() {
+            l if l > 4 => w.chars().rev().collect(),
+            _ => w.to_string(),
+        })
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
+#[cfg(test)]
+mod test_spin_words {
+    use super::*;
+
+    #[test]
+    fn examples() {
+        assert_eq!(spin_words("Welcome"), "emocleW");
+        assert_eq!(spin_words("Hey fellow warriors"), "Hey wollef sroirraw");
+        assert_eq!(spin_words("This is a test"), "This is a test");
+        assert_eq!(spin_words("This is another test"), "This is rehtona test");
+        assert_eq!(
+            spin_words("You are almost to the last test"),
+            "You are tsomla to the last test"
+        );
+        assert_eq!(
+            spin_words("Just kidding there is still one more"),
+            "Just gniddik ereht is llits one more"
+        );
+        assert_eq!(
+            spin_words("Seriously this is the last one"),
+            "ylsuoireS this is the last one"
+        );
+    }
+}
+
+fn decode_morse(encoded: &str) -> String {
+    let morse_code: HashMap<&'static str, String> = Default::default();
+
+    encoded
+        .trim()
+        .split("   ")
+        .map(|w| {
+            w.split_whitespace()
+                .map(|letter| morse_code.get(letter).unwrap().clone())
+                .collect::<String>()
+        })
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
+fn in_array(arr_a: &[&str], arr_b: &[&str]) -> Vec<String> {
+    let mut substrings = arr_a
+        .iter()
+        .filter_map(|&a_str| {
+            if arr_b
+                .iter()
+                .filter(|&b_str| b_str.len() >= a_str.len())
+                .any(|&b_str| b_str.contains(a_str))
+            {
+                Some(a_str.to_string())
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<String>>();
+
+    substrings.sort();
+    substrings.dedup();
+
+    substrings
+}
+
+#[cfg(test)]
+mod test_in_array {
+    use super::*;
+
+    #[test]
+    fn examples() {
+        assert_eq!(
+            in_array(
+                &["xyz", "live", "strong"],
+                &["lively", "alive", "harp", "sharp", "armstrong"],
+            ),
+            ["live", "strong"]
+        );
+
+        assert_eq!(
+            in_array(
+                &["live", "strong", "arp"],
+                &["lively", "alive", "harp", "sharp", "armstrong"],
+            ),
+            ["arp", "live", "strong"]
+        );
+
+        assert_eq!(
+            in_array(
+                &["tarp", "mice", "bull"],
+                &["lively", "alive", "harp", "sharp", "armstrong"],
+            ),
+            [] as [&str; 0]
+        );
+
+        assert_eq!(
+            in_array(
+                &["live", "strong", "arp", "arp"],
+                &["lively", "alive", "harp", "sharp", "armstrong"],
+            ),
+            ["arp", "live", "strong"]
+        );
+    }
+}
+
+fn bouncing_ball(h: f64, bounce: f64, window: f64) -> i32 {
+    if h <= 0.0 || bounce < 0.0 || bounce >= 1.0 || window >= h {
+        return -1;
+    }
+
+    let mut bounces = 1;
+    let mut h = h;
+
+    while h > window {
+        // bounce
+        h = h * bounce;
+
+        if h > window {
+            bounces += 2;
+        }
+    }
+
+    bounces
+}
+
+fn testequal(h: f64, bounce: f64, window: f64, exp: i32) -> () {
+    assert_eq!(bouncing_ball(h, bounce, window), exp)
+}
+
+#[test]
+fn tests_bouncing_ball() {
+    testequal(3.0, 0.66, 1.5, 3);
+    testequal(30.0, 0.66, 1.5, 15);
+    testequal(40.0, 0.4, 10.0, 3);
+    testequal(10.0, 0.6, 10.0, -1);
 }
